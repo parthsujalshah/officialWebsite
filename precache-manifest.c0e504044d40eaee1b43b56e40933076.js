06dc97c2095182c9c1a7bcb2b3b3cde4671e9abc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 11:34:14 +0000

    Deploy developer-student-club-thapar/officialWebsite to github.com/developer-student-club-thapar/officialWebsite.git:gh-pages

---
+++ precache-manifest.c0e504044d40eaee1b43b56e40933076.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897e226b60b5607c3790f2d59fae9af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5f1ff5ef4f24ba80f5d",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "25f5e381b152115cd19b",
+    "url": "/static/css/main.904405ec.chunk.css"
+  },
+  {
+    "revision": "f5f1ff5ef4f24ba80f5d",
+    "url": "/static/js/2.5981b179.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/static/js/2.5981b179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f5e381b152115cd19b",
+    "url": "/static/js/main.f8ba0ed1.chunk.js"
+  },
+  {
+    "revision": "6eaca34e982eaee65e1d",
+    "url": "/static/js/runtime-main.6417d7bf.js"
+  },
+  {
+    "revision": "e60eac57071bede7ed7ff214aad5e4b7",
+    "url": "/static/media/Projects.e60eac57.svg"
+  },
+  {
+    "revision": "0e8afb5c697b412bf2b8b61f3ba0640d",
+    "url": "/static/media/Sponsership.0e8afb5c.svg"
+  },
+  {
+    "revision": "c7f8f0025f585bbf8abebfeae1a50cfe",
+    "url": "/static/media/ThaparUniversity_1.c7f8f002.jpg"
+  },
+  {
+    "revision": "0d517ed634ef717d1e40123b3af77c11",
+    "url": "/static/media/dsc_logo.0d517ed6.png"
+  },
+  {
+    "revision": "985f281c56fbd532a69b4d448ec926a1",
+    "url": "/static/media/events.985f281c.svg"
+  },
+  {
+    "revision": "6954b6f2e94786ed6fe88c90e4360d16",
+    "url": "/static/media/googlefinal.6954b6f2.ico"
+  },
+  {
+    "revision": "b6e2ce1534e3527d7f96b2dd03a1b05d",
+    "url": "/static/media/header.b6e2ce15.png"
+  },
+  {
+    "revision": "c557ab2812f206956d2e66e9ef2dcdb3",
+    "url": "/static/media/projects.c557ab28.jpg"
+  },
+  {
+    "revision": "874007553b9074767c5dd2fc274ef157",
+    "url": "/static/media/slack.87400755.png"
+  },
+  {
+    "revision": "31c7575f4131ca3efbee3e6137f9cf93",
+    "url": "/static/media/team.31c7575f.webp"
+  },
+  {
+    "revision": "f47cc4276117e6f91d56408f3a30b025",
+    "url": "/static/media/undraw_Firmware_jw6u.f47cc427.svg"
+  },
+  {
+    "revision": "a217b34f95a8df63091b7a5d9777a643",
+    "url": "/static/media/undraw_Growing_qwt2.a217b34f.svg"
+  },
+  {
+    "revision": "a84f8fb2b7389ab439fa6b5ec1768908",
+    "url": "/static/media/undraw_digital_currency_qpak.a84f8fb2.svg"
+  },
+  {
+    "revision": "f1347d6d74e36958a2226ef418453453",
+    "url": "/static/media/undraw_positive_attitude_xaae.f1347d6d.svg"
+  },
+  {
+    "revision": "cb01a9a5460a56030fed345449274610",
+    "url": "/static/media/undraw_programming_2svr.cb01a9a5.svg"
+  },
+  {
+    "revision": "3ca8774fde5541e02ae7f9de9efb4bad",
+    "url": "/static/media/undraw_progressive_app_m9ms.3ca8774f.svg"
+  },
+  {
+    "revision": "fd1f60938ba1426cc566dbcdcb80b216",
+    "url": "/static/media/undraw_scrum_board_cesn.fd1f6093.svg"
+  },
+  {
+    "revision": "20c529007a55005e6726027bc7997229",
+    "url": "/static/media/unnamed.20c52900.png"
+  }
+]);
